4387f037fcb99c06759d1b3eb31eeacd1a13038e
Chandra Wijaya <chandrapinter25@gmail.com>
2026-05-02 12:47:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxx/exec
 
 window.GYM_CONFIG = {
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyMCKTi79ch6eX-wDBTxD_92LkZsoEorziLrWLenV_fhrtREHKKuj8UO52UA3LDnFJtvg/exec",
+  SCRIPT_URL:"https://script.google.com/macros/s/AKfycbwZUKBelzRYWQdUfVKeaAKBYExTe4GkwaAxqeQqso3p6qKuzOnQ65Owuc07Sz44rHz1qw/exec",
   APP_NAME: "Sistem Admin Gym",
   GYM_NAME: "SISTEM FC",
   REFRESH_INTERVAL_MS: 10000,
